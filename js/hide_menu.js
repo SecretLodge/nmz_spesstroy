341b024898fb837bf45a65aa0c3f98f25e8c1b85
dntuchmeplz <boxofmonster@mail.ru>
2024-06-03 02:39:02 +0500

    fix bug with mobile menu height

--- js/hide_menu.js
+++ js/hide_menu.js
@@ -4,9 +4,11 @@ const open = document.getElementById('open');
 open.addEventListener('click', () => {
     const hide = document.getElementById('hide');
 
-menu.classList.remove('menu-mobile__hide');
+    menu.classList.remove('menu-mobile__hide');
+    document.body.classList.add('body__mobile-open');
 
     hide.addEventListener('click', () => {
         menu.classList.add('menu-mobile__hide');
+        document.body.classList.remove('body__mobile-open')
     });
 });
